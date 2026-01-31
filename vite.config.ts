fba7f7dc446929f15a30595652b2779778f3b47e
judha <judha@itoi.org>
2026-01-31 18:47:03 +0530

    feat: Add an Axios API utility and configure Vite proxy to point to the production backend URL.

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
     proxy: {
+      // '/api': 'http://localhost:3001'
       '/api': 'https://fgpcngl-platform.onrender.com'
     }
   },
